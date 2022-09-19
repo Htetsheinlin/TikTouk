d3f1deff7d8e96fd2a17eac0275c41b0113f9dcd
Htetsheinlin <44019910+Htetsheinlin@users.noreply.github.com>
2022-09-19 20:43:17 +0630

    Md to md

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react'
-import { MdFavorite } from 'react-icons/Md'
+import { MdFavorite } from 'react-icons/md'
 import useAuthStore from '../store/authStore'
 
 interface Iprops {
